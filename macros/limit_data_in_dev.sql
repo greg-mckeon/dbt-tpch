104910b1c151e766b292ac9d973357ffaea5de9e
Greg McKeon <greg.mckeon@dbtlabs.com>
2023-08-24 23:51:43 +0000

    this is a new commit

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,10 +1,6 @@
 {% macro limit_data_in_dev(filter_column_name, lookback_days=7) %}
 
-{% if target.name == default %}
-
-
-where {{ filter_column_name }} >= dateadd('day', -{{ lookback_days }}, current_timestamp)
-{% endif %}
+{% if target.name editing stuff
 
   {% do log(filter_column_name , True) %}
 
